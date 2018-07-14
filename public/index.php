4605e338186103957cce07c4746e404024cddba9
Stanislas Bernard <stanislasb@theodo.fr>
2018-07-14 10:30:28 +0200

    Pages are php templates

--- public/index.php
+++ public/index.php
@@ -16,7 +16,9 @@ $map = [
 $path = $request->getPathInfo();
 
 if (isset($map[$path])) {
+    ob_start();
     require $map[$path];
+    $response->setContent(ob_get_clean());
 } else {
     $response->setStatusCode(404);
     $response->setContent('Not Found');
